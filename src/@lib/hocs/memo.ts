49aa6679012a16e48eaba2ea9287a619687f9586
배규덕 <mandarinduk@gmail.com>
2025-04-09 19:17:54 +0900

    feat: memo  구현

--- src/@lib/hocs/memo.ts
+++ src/@lib/hocs/memo.ts
@@ -1,10 +1,31 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import { shallowEquals } from "../equalities";
-import { ComponentType } from "react";
+import React, { ComponentType } from "react";
 
 export function memo<P extends object>(
   Component: ComponentType<P>,
   _equals = shallowEquals,
 ) {
-  return Component;
+  let memoizedProps: P | null = null;
+  let MemoizedComponent: React.ReactElement | null = null;
+
+  const updateComponent = (props: P) => {
+    memoizedProps = props;
+    MemoizedComponent = React.createElement(Component, props);
+  };
+
+  const WrappedComponent = (props: P) => {
+    if (!MemoizedComponent) {
+      updateComponent(props);
+      return MemoizedComponent;
+    }
+
+    if (_equals(props, memoizedProps)) {
+      return MemoizedComponent;
+    }
+
+    updateComponent(props);
+    return MemoizedComponent;
+  };
+
+  return WrappedComponent;
 }
